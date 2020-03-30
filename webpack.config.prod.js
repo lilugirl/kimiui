f7c8f08d0199c6db6581dad412ed8dd4e73b9caa
刘毅 <liuyi1@chint.com>
2020-03-30 11:10:43 +0800

    webpack

---
+++ webpack.config.prod.js
@@ -0,0 +1,54 @@
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+var path = require("path");
+module.exports = {
+  mode: "none",
+  devtool: "cheap-module-source-map",
+  entry: {
+    main: "./src/index.js"
+  },
+  output: {
+    publicPath: "/",
+    path: path.resolve(__dirname, "./dist"),
+    filename: "[name]-bundle.js"
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+      filename: "index.html",
+      chunks: ["main"]
+    }),
+    new CleanWebpackPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      },
+      {
+        test: /\.(jpg|png|gif)$/,
+        use: {
+          loader: "url-loader",
+          options: {
+            name: "[name]_[hash].[ext]",
+            outputPath: "images/",
+            limit: 10240
+          }
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.(eot|ttf|svg)$/,
+        use: {
+          loader: "file-loader"
+        }
+      }
+    ]
+  }
+};
